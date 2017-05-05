f3ba0234b81155bbddf2319fa415e76467613bd7
Abdou00 <abdoulaye_diarra@outlook.fr>
2017-05-05 11:05:07 +0200

    Débuggage assets

--- config/assets/production.js
+++ config/assets/production.js
@@ -1,25 +1,70 @@
+// 'use strict';
+//
+// module.exports = {
+//   client: {
+//     lib: {
+//       css: [
+//         'public/lib/bootstrap/dist/css/bootstrap.min.css',
+//         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+//       ],
+//       js: [
+//         'public/lib/angular/angular.min.js',
+//         'public/lib/angular-resource/angular-resource.min.js',
+//         'public/lib/angular-animate/angular-animate.min.js',
+//         'public/lib/angular-messages/angular-messages.min.js',
+//         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+//         'public/lib/angular-ui-utils/ui-utils.min.js',
+//         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+//         'public/lib/angular-file-upload/angular-file-upload.min.js',
+//         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+//       ]
+//     },
+//     css: 'public/dist/application.min.css',
+//     js: 'public/dist/application.min.js'
+//   }
+// };
+
 'use strict';
 
 module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
       ],
       js: [
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
-      ]
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-leaflet-directive/dist/angular-leaflet-directive.min.js'
+      ],
+      tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
-    css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js'
+    css: [
+      'modules/*/client/css/*.css',
+      'https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.css'
+    ],
+    less: [
+      'modules/*/client/less/*.less'
+    ],
+    sass: [
+      'modules/*/client/scss/*.scss'
+    ],
+    js: [
+      'modules/core/client/app/config.js',
+      'modules/core/client/app/init.js',
+      'modules/*/client/*.js',
+      'modules/*/client/**/*.js',
+      'https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.js'
+    ],
+    views: ['modules/*/client/views/**/*.html'],
+    templates: ['build/templates.js']
   }
-};
+}
