e386e360583159900fd2fc459569f5f68a0f11d2
Abdou00 <abdoulaye_diarra@outlook.fr>
2017-05-31 16:10:02 +0200

    Fix bug employee policy

--- modules/teams/server/routes/teams.server.routes.js
+++ modules/teams/server/routes/teams.server.routes.js
@@ -5,9 +5,9 @@
 */
 var path = require('path'),
   teamsPolicy = require('../policies/teams.server.policy'),
-  employeesPolicy = require(path.resolve('../vlg-espaces-verts/modules/employees/server/policies/employees.server.policy')),
-  teams = require('../controllers/teams.server.controller'),
-  employees = require(path.resolve('../vlg-espaces-verts/modules/employees/server/controllers/employees.server.controller'));
+  // employeesPolicy = require(path.resolve('../vlg-espaces-verts/modules/employees/server/policies/employees.server.policy')),
+  teams = require('../controllers/teams.server.controller');
+  // employees = require(path.resolve('../vlg-espaces-verts/modules/employees/server/controllers/employees.server.controller'));
 
 module.exports = function(app) {
   // Teams Routes
@@ -21,13 +21,13 @@ module.exports = function(app) {
     .put(teams.update)
     .delete(teams.delete);
 
-  app.route('/api/employees').all(employeesPolicy.isAllowed)
-    .get(employees.list);
-
-  app.route('/api/employees/:employeeId').all(employeesPolicy.isAllowed)
-    .get(employees.read)
-    .put(employees.update)
-    .delete(employees.delete);
+  // app.route('/api/employees').all(employeesPolicy.isAllowed)
+  //   .get(employees.list);
+  //
+  // app.route('/api/employees/:employeeId').all(employeesPolicy.isAllowed)
+  //   .get(employees.read)
+  //   .put(employees.update)
+  //   .delete(employees.delete);
 
   // Finish by binding the Team middleware
   app.param('teamId', teams.teamByID);
