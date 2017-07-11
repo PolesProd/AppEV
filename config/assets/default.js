15e14957615e2941330ee9075c48890ceb7d6064
Abdou00 <abdoulaye_diarra@outlook.fr>
2017-07-11 15:12:00 +0200

    Fix bug heroku master

--- config/assets/default.js
+++ config/assets/default.js
@@ -6,6 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/isteven-angular-multiselect/isteven-multi-select.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +17,8 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/isteven-angular-multiselect/isteven-multi-select.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
