ce814412e2ee4c988af7971cd5047695b08170d6
Abdou00 <abdoulaye_diarra@outlook.fr>
2017-08-09 12:16:30 +0200

    Ajout condition du header

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,37 +1,38 @@
 'use strict';
 
 angular.module('core').controller('HeaderController', ['$scope', '$state', '$location', 'Authentication', 'Menus',
-  function ($scope, $state, $location, Authentication, Menus) {
-    // Expose view variables
-    $scope.$state = $state;
-    $scope.authentication = Authentication;
+function ($scope, $state, $location, Authentication, Menus) {
+  // Expose view variables
+  $scope.$state = $state;
+  $scope.authentication = Authentication;
 
 
-    // Get the topbar menu
-    $scope.menu = Menus.getMenu('topbar');
+  // Get the topbar menu
+  $scope.menu = Menus.getMenu('topbar');
 
-    // Toggle the menu items
+  // Toggle the menu items
+  $scope.isCollapsed = false;
+  $scope.toggleCollapsibleMenu = function () {
+    $scope.isCollapsed = !$scope.isCollapsed;
+  };
+
+  // Collapsing the menu after navigation
+  $scope.$on('$stateChangeSuccess', function () {
     $scope.isCollapsed = false;
-    $scope.toggleCollapsibleMenu = function () {
-      $scope.isCollapsed = !$scope.isCollapsed;
-    };
+  });
 
-    // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = false;
-    });
+  var url = '';
+  url = $location.path();
 
+  var headerStyle = document.getElementById('header');
 
-
-    // function headerStyle() {
-    //   var url = $location.url();
-    //   var header = angular.element(document.querySelector('#header'));
-    //
-    //   if (url !== '/teams') {
-    //     headerStyle.css('width', '40%');
-    //   }
-    //   headerStyle.css('width', '100%');
-    //   console.log(url);
-    // }
+  if (url === '/employes') {
+    headerStyle.style.width = '100%';
+  } else if (url === '/inventories') {
+    headerStyle.style.width = '100%';
+  } else if (url === '/plannings') {
+    headerStyle.style.width = '100%';
   }
+  console.log(url);
+}
 ]);
